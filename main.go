e3bbface65da7d5405522c2a36347b197e2773b4
Eddie Dinel <edinel@solace.org>
2023-03-27 17:45:35 -0700

    pushing

--- main.go
+++ main.go
@@ -92,7 +92,9 @@ func tideHandler(tidesapi *tides.Client) http.HandlerFunc {
 		}
 
 		buf := &bytes.Buffer{}
+		fmt.Printf("\n%+v", *tideQ)
 		err = tpl.Execute(buf, tideQ)
+
 		fmt.Printf("\n\nError: %s\n\n", err.Error())
 
 		if err != nil {
